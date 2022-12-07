48276d319960cb63a5b59c2e85cd0b1e13007a61
Ryo Nihei <nihei.dev@gmail.com>
2022-12-08 00:50:35 +0900

    Refactor

--- src/main/scala/Parsers.scala
+++ src/main/scala/Parsers.scala
@@ -1,52 +1,14 @@
 import java.io.Reader
 import scala.util.parsing.combinator.RegexParsers
-import org.json4s.JValue
-import org.json4s.JsonDSL._
-
-trait UCDElement {
-  def serialize: JValue
-}
-
-case class CodePoint(p: Int) extends UCDElement {
-  def serialize: JValue = p
-}
-
-case class CodePointRange(
-  start: CodePoint,
-  end: CodePoint
-) extends UCDElement {
-  def serialize: JValue = Map(
-    "s" -> start.serialize,
-    "e" -> end.serialize
-  )
-}
-
-case class MissingLine(
-  cpRange: CodePointRange,
-  field: String,
-  rest: List[String]
-) extends UCDElement {
-  def serialize: JValue = ("cp" -> cpRange.serialize) ~~ ("default" -> (field::rest))
-}
-
-case class Line(
-  fields: Option[UCDElement],
-  missingLine: Option[MissingLine]
-) extends UCDElement {
-  def serialize: JValue = {
-    ("fields" ->
-      (fields match
-        case Some(fs) => Some(fs.serialize)
-        case None => None))
-    ~~ ("missingLine" ->
-      (missingLine match
-        case Some(ml) => Some(ml.serialize)
-        case None => None))
-  }
-}
 
 trait UCDParsers extends RegexParsers {
   override val whiteSpace = """[ \t\x0B\f\r]""".r
+
+  def apply(src: Reader): Either[String, List[Line]] =
+    parseAll(ucd, src) match {
+      case Success(result, _) => Right(result)
+      case NoSuccess(error, _) => Left(error)
+    }
 
   def ucd: Parser[List[Line]] = rep(line)
   def line: Parser[Line] = (fields ~! opt(comment) | comment) <~! opt("""\n+""".r) ^^ {
@@ -78,38 +40,11 @@ trait UCDParsers extends RegexParsers {
   }
 }
 
-case class PropertyValueAliasesFields(
-  propertyName: String,
-  propertyValueNameFormal: String,
-  propertyValueNameAliases: List[String],
-) extends UCDElement {
-  def serialize: JValue = {
-    ("name" -> propertyName)
-    ~~ ("formal" -> propertyValueNameFormal)
-    ~~ ("aliases" -> propertyValueNameAliases)
-  }
-}
-
 object PropertyValueAliases extends UCDParsers {
   override def fields: Parser[PropertyValueAliasesFields] = string ~! sepString ~! sepString ~! rep(sepString) ^^ {
     case f1 ~ f2 ~ f3 ~ rest =>
       if f1 == "ccc" then PropertyValueAliasesFields(f1, rest.head, f2::f3::rest.tail)
       else PropertyValueAliasesFields(f1, f3, f2::rest)
-  }
-  def apply(src: Reader): Either[String, List[Line]] =
-    parseAll(ucd, src) match {
-      case Success(result, _) => Right(result)
-      case NoSuccess(error, _) => Left(error)
-    }
-}
-
-case class UnicodeDataFields(
-  codePoint: CodePoint,
-  generalCategory: Option[String]
-) extends UCDElement {
-  def serialize: JValue = {
-    ("cp" -> codePoint.serialize)
-    ~~ ("gc" -> generalCategory)
   }
 }
 
@@ -117,9 +52,4 @@ object UnicodeData extends UCDParsers {
   override def fields: Parser[UnicodeDataFields] = codePoint ~! rep(sepOptString) ^^ {
     case cp ~ fs => UnicodeDataFields(cp, fs(1))
   }
-  def apply(src: Reader): Either[String, List[Line]] =
-    parseAll(ucd, src) match {
-      case Success(result, _) => Right(result)
-      case NoSuccess(error, _) => Left(error)
-    }
 }
