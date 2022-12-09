406186edeb683d528c2fdf2e549d88cc12edc592
Ryo Nihei <nihei.dev@gmail.com>
2022-12-10 00:17:49 +0900

    Close resources with Using

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,25 @@
+import scala.util.{Try, Using}
 import scala.io.Source
 import org.json4s.JValue
 import org.json4s.JsonDSL._
 import org.json4s.native.JsonMethods
 
 @main def ucdx: Unit = {
-  val propValAliases = Source.fromURL("https://www.unicode.org/Public/15.0.0/ucd/PropertyValueAliases.txt")
-  PropertyValueAliases(propValAliases.reader) match {
-    case Right(result) => println(JsonMethods.compact(JsonMethods.render(serialize(result))))
-    case Left(error) => println(error)
+  Using(Source.fromURL(ucdFileURL("PropertyValueAliases.txt"))) { src =>
+    PropertyValueAliases(src.reader) match {
+      case Right(result) => println(JsonMethods.compact(JsonMethods.render(serialize(result))))
+      case Left(error) => println(error)
+    }
   }
-  propValAliases.close()
 
-  val uniData = Source.fromURL("https://www.unicode.org/Public/15.0.0/ucd/UnicodeData.txt")
-  UnicodeData(uniData.reader) match {
-    case Right(result) => println(JsonMethods.compact(JsonMethods.render(serialize(result))))
-    case Left(error) => println(error)
+  Using(Source.fromURL(ucdFileURL("UnicodeData.txt"))) { src =>
+    UnicodeData(src.reader) match {
+      case Right(result) => println(JsonMethods.compact(JsonMethods.render(serialize(result))))
+      case Left(error) => println(error)
+    }
   }
-  uniData.close()
 }
 
 def serialize(lines: List[Line]): JValue = lines.map(line => line.serialize)
+
+def ucdFileURL(fileName: String): String = s"https://www.unicode.org/Public/15.0.0/ucd/${fileName}"
