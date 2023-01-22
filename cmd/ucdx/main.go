bdce91bbc8078a5954fd70b33ea34bf928ef0942
Ryo Nihei <nihei.dev@gmail.com>
2023-01-23 01:57:01 +0900

    Update

--- cmd/ucdx/main.go
+++ cmd/ucdx/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
+	"github.com/adrg/xdg"
 	"github.com/nihei9/ucdx/ucd"
 )
 
@@ -18,44 +19,58 @@ func main() {
 }
 
 func gen() error {
-	var propValAliases *ucd.File[*ucd.PropertyValueAliasesRecord, *ucd.PropertyValueAliasesDefaultRecord]
 	{
-		log.Println("Get PropertyValueAliases.txt")
 		resp, err := http.Get("https://www.unicode.org/Public/13.0.0/ucd/PropertyValueAliases.txt")
 		if err != nil {
 			return err
 		}
 		defer resp.Body.Close()
-		log.Println("OK")
-		log.Println("Start parse")
-		propValAliases, err = ucd.Parse(resp.Body, ucd.NewPropertyValueAliasesParser())
+		f, err := ucd.Parse(resp.Body, ucd.NewPropertyValueAliasesParser())
 		if err != nil {
 			return err
 		}
-		log.Print("Finished")
-	}
-	// var unicodeData *ucd.File[*ucd.UnicodeDataRecord, *ucd.UnicodeDataDefaultRecord]
-	// {
-	// 	resp, err := http.Get("https://www.unicode.org/Public/13.0.0/ucd/UnicodeData.txt")
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	defer resp.Body.Close()
-	// 	unicodeData, err = ucd.Parse(resp.Body, ucd.NewUnicodeDataParser())
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
-	for i, rec := range propValAliases.Records {
-		fmt.Printf("%5v: %v; %v; %v", i, rec.PropertyName, rec.ShortName, rec.LongName)
-		for _, o := range rec.Others {
-			fmt.Printf("; %v", o)
+		cachePath, err := xdg.CacheFile("ucdx/PropertyValueAliases.gob")
+		if err != nil {
+			return err
 		}
-		fmt.Print("\n")
+		out, err := os.OpenFile(cachePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+		gob.NewEncoder(out).Encode(f)
 	}
-	for i, rec := range propValAliases.Defaults {
-		fmt.Printf("%5v: %v; %v; %v\n", i, rec.CodePoint, rec.PropertyName, rec.ValueName)
+	{
+		resp, err := http.Get("https://www.unicode.org/Public/13.0.0/ucd/UnicodeData.txt")
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		f, err := ucd.Parse(resp.Body, ucd.NewUnicodeDataParser())
+		if err != nil {
+			return err
+		}
+		cachePath, err := xdg.CacheFile("ucdx/UnicodeData.gob")
+		if err != nil {
+			return err
+		}
+		out, err := os.OpenFile(cachePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+		gob.NewEncoder(out).Encode(f)
 	}
+	// for i, rec := range propValAliases.Records {
+	// 	fmt.Printf("%5v: %v; %v; %v", i, rec.PropertyName, rec.ShortName, rec.LongName)
+	// 	for _, o := range rec.Others {
+	// 		fmt.Printf("; %v", o)
+	// 	}
+	// 	fmt.Print("\n")
+	// }
+	// for i, rec := range propValAliases.Defaults {
+	// 	fmt.Printf("%5v: %v; %v; %v\n", i, rec.CodePoint, rec.PropertyName, rec.ValueName)
+	// }
 	// for i, rec := range unicodeData.Records {
 	// 	fmt.Printf("%5v: %v; gc=%v\n", i, rec.CodePoint, rec.GeneralCategory)
 	// }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/nihei9/ucdx
 
 go 1.19
+
+require (
+	github.com/adrg/xdg v0.4.0 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
+)
