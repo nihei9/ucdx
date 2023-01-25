480177740d4af09c946bdf5e976d0689fb48aafd
Ryo Nihei <nihei.dev@gmail.com>
2023-01-26 00:47:12 +0900

    Use Unicode 15.0.0

--- ucd/helper.go
+++ ucd/helper.go
@@ -2,6 +2,8 @@ package ucd
 
 import "fmt"
 
+const unicodeVersion = "15.0.0"
+
 func NewUCDFileURL(ucdFileName string) string {
-	return fmt.Sprintf("https://www.unicode.org/Public/13.0.0/ucd/%v", ucdFileName)
+	return fmt.Sprintf("https://www.unicode.org/Public/%v/ucd/%v", unicodeVersion, ucdFileName)
 }
